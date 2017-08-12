6793be14bd4d33c8dfa421e1db1ce14a733fb67f
David Perreault <davidperreault@hotmail.com>
2017-08-12 14:35:28 -0400

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAn6F7Pr45i2_WV4vWrZrq4oO6iWAX09gI",
+    databaseURL: "https://todo-firebase-d4f17.firebaseio.com",
+    projectId: "todo-firebase-d4f17"
+      }
+};
